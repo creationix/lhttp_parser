e5b33b9b452da8c943dfce9352b119e3f5263c3a
Dennis Schridde <devurandom@gmx.net>
2012-12-06 19:11:32 +0100

    Lua 5.2 compatibility with fallbacks for Lua 5.1
    Use uservalues instead of environments.
      This changes behaviour, but achieves the same goal.
    
    Use luaL_newlib/setfuncs instead of luaL_register,
      but provide a fallback for Lua 5.1 for the former.

--- lhttp_parser.h
+++ lhttp_parser.h
@@ -21,6 +21,16 @@
 #include "lua.h"
 #include "lauxlib.h"
 
+#if LUA_VERSION_NUM < 502
+/* lua_rawlen: Not entirely correct, but should work anyway */
+#	define lua_rawlen lua_objlen
+/* lua_...uservalue: Something very different, but it should get the job done */
+#	define lua_getuservalue lua_getfenv
+#	define lua_setuservalue lua_setfenv
+#	define luaL_newlib(L,l) (lua_newtable(L), luaL_register(L,NULL,l))
+#	define luaL_setfuncs(L,l,n) (assert(n==0), luaL_register(L,NULL,l))
+#endif
+
 LUALIB_API int luaopen_lhttp_parser (lua_State *L);
 
 #endif
